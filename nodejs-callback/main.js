a9b10dcc827dbed45a796ed568124bd4f0b55da7
wzwang <wzwang@Ctrip.com>
2016-09-01 15:50:37 +0800

    add about callback

--- nodejs-callback/main.js
+++ nodejs-callback/main.js
@@ -1,7 +1,16 @@
 /**
  * Created by wzwang on 2016/8/30.
  */
+//var fs = require("fs");
+//var data = fs.readFileSync("input.txt");
+//console.log(data.toString());
+//console.log("程序执行结束~");
+
 var fs = require("fs");
-var data = fs.readFileSync("input.txt");
-console.log(data.toString());
-console.log("程序执行结束~");
+fs.readFile("input.txt", function (err, data) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log(data.toString());
+});
+console.log("che");
